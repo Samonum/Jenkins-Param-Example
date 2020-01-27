5c7235c49be85427c4b34381322a78e658bcdb0f
Sam <sam_mie_sam@hotmail.com>
2020-01-27 16:03:12 +0100

    Added data check to unit tests

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -6,6 +6,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.After;
 import static org.junit.Assert.*;
+import java.util.Properties;
+import java.io.InputStream;
+import java.io.IOException;
 
 /**
  * Unit test for simple App.
@@ -20,6 +23,23 @@ public class AppTest
         System.setOut(new PrintStream(outContent));
     }
 
+    @Test
+    public void testDataSet() {
+        try (InputStream input = getClass()
+			.getClassLoader().getResourceAsStream("config.properties")) {
+
+            Properties prop = new Properties();
+
+            // Load a properties file and read the data variable
+            prop.load(input);
+			
+			assertNotNull("The data property is not set.", prop.getProperty("data"));
+
+        } catch (IOException ex) {
+            fail("An IOException occured trying to load the config file.")
+        }
+    }
+	
     @Test
     public void testAppConstructor() {
         try {
